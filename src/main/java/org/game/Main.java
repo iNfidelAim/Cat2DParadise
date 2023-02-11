e1c4cf7d5d7eb9bf5024c54c0c8d970bb1426d2b
Ruslan Afanasiev <ruslanafanasiev555@gmail.com>
2023-02-11 19:51:31 +0700

    Start

--- src/main/java/org/game/Main.java
+++ src/main/java/org/game/Main.java
@@ -2,6 +2,6 @@ package org.game;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Game for you");
     }
 }
