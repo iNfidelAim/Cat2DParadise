2b56c26855c210b9f95d202c72094a20bfc4fb81
Ruslan Afanasiev <ruslanafanasiev555@gmail.com>
2023-02-21 19:42:01 +0700

    Импортировал файлы шрифтов в сам проект, чтобы не было локальной зависимоти и исключения по этому поводу. Сам шрифт еще не подобрал и просто засунул обычный arial

--- src/main/java/org/game/main/UI.java
+++ src/main/java/org/game/main/UI.java
@@ -4,13 +4,15 @@ import org.game.object.OBJ_Key;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.DecimalFormat;
 
 public class UI {
 
     GamePanel gp;
     Graphics2D g2;
-    Font arial_40, arial_80B;
+    Font arial;
     public boolean messageOn = false;
     public String message = "";
     int messageCounter = 0;
@@ -20,8 +22,14 @@ public class UI {
 
     public UI(GamePanel gp) {
         this.gp = gp;
-        arial_40 = new Font("Arial", Font.PLAIN, 32);
-        arial_80B = new Font("Arial", Font.BOLD, 80);
+
+        try {
+            InputStream inputStream = getClass().getResourceAsStream("/font/arial.ttf");
+            arial = Font.createFont(Font.TRUETYPE_FONT, inputStream);
+        } catch (FontFormatException | IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void showMessage(String text) {
@@ -31,7 +39,7 @@ public class UI {
     public void draw(Graphics2D g2) {
 
         this.g2 = g2;
-        g2.setFont(arial_40);
+        g2.setFont(arial);
         g2.setColor(Color.WHITE);
         //PLAY STATE
         if(gp.gameState == gp.playState) { /*do play staff later*/ }
